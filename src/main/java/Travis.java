7160da0485edc8f70077c5cead7ed4d18f15dac6
203ZK <houjinwork@gmail.com>
2025-08-14 17:11:05 +0800

    Finish Level-0.
    Change the bot's name to Travis and create a skeletal version.

---
+++ src/main/java/Travis.java
@@ -0,0 +1,10 @@
+public class Travis {
+    public static void main(String[] args) {
+        String HORIZONTAL_LINE = "____________________________________________________________";
+        String introduction = "\nHello! I'm Travis\n";
+        String greeting = "What can I do for you?\n";
+        String farewell = "\nBye. Hope to see you again soon!\n";
+
+        System.out.println(HORIZONTAL_LINE + introduction + greeting + HORIZONTAL_LINE + farewell + HORIZONTAL_LINE);
+    }
+}
