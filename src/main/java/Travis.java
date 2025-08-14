092c141ca4f34f4007db1567536a23dabc0fc520
203ZK <houjinwork@gmail.com>
2025-08-14 17:52:36 +0800

    Finish Level-2.
    Enable the creation and listing of tasks.

--- src/main/java/Travis.java
+++ src/main/java/Travis.java
@@ -1,18 +1,39 @@
 import java.util.Scanner;
 
 public class Travis {
+    private final TodoList todos;
+
+    public Travis() {
+        this.todos = new TodoList();
+    }
+
     public static void main(String[] args) {
-        String greeting = "Hello! I'm Travis\nWhat can I do for you?";
-        String farewell = "Bye. Hope to see you again soon!";
-        System.out.println(wrapInHorizontalLines(greeting));
+        Travis travis = new Travis();
+        greet();
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(wrapInHorizontalLines(input + "\nOh don't be boring, say something else!"));
+            if (input.equals("list")) {
+                System.out.println(wrapInHorizontalLines(travis.todos.toString()));
+            } else {
+                String addedTask = travis.todos.addTask(input);
+                System.out.println(wrapInHorizontalLines(addedTask));
+            }
             input = scanner.nextLine();
         }
-        System.out.println(wrapInHorizontalLines(farewell));
+
+        farewell();
+    }
+
+    private static void greet() {
+        String GREETING = "Hello! I'm Travis!\nWhat can I do for you?";
+        System.out.println(wrapInHorizontalLines(GREETING));
+    }
+
+    private static void farewell() {
+        String FAREWELL = "Bye. Hope to see you again soon!";
+        System.out.println(wrapInHorizontalLines(FAREWELL));
     }
 
     private static String wrapInHorizontalLines(String content) {
