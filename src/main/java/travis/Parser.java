748ded0cd870a996c89b6828e8ed33e322abd580
203ZK <houjinwork@gmail.com>
2025-08-25 08:36:08 +0800

    Refactor reading user inputs into Parser class

---
+++ src/main/java/travis/Parser.java
@@ -0,0 +1,35 @@
+package travis;
+
+import travis.chatbot.Travis;
+import travis.constants.Enums;
+import travis.constants.RegexConstants;
+import travis.exceptions.InvalidTaskException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+    private static final Pattern markAsDonePattern = Pattern.compile(RegexConstants.MARK_AS_DONE_REGEX);
+    private static final Pattern markAsNotDonePattern = Pattern.compile(RegexConstants.MARK_AS_NOT_DONE_REGEX);
+    private static final Pattern deleteTaskPattern = Pattern.compile(RegexConstants.DELETE_TASK_REGEX);
+
+    public static void parse(Travis travis, String input) {
+        Matcher markAsDoneMatcher = markAsDonePattern.matcher(input);
+        Matcher markAsNotDoneMatcher = markAsNotDonePattern.matcher(input);
+        Matcher deleteTaskMatcher = deleteTaskPattern.matcher(input);
+
+        if (markAsDoneMatcher.matches()) {
+            travis.markTaskAsDone(markAsDoneMatcher.group(Enums.RegexGroup.TASK_INDEX.getGroup()));
+        } else if (markAsNotDoneMatcher.matches()) {
+            travis.markTaskAsNotDone(markAsNotDoneMatcher.group(Enums.RegexGroup.TASK_INDEX.getGroup()));
+        } else if (deleteTaskMatcher.matches()) {
+            travis.deleteTask(deleteTaskMatcher.group(Enums.RegexGroup.TASK_INDEX.getGroup()));
+        } else if (input.equals("list")) {
+            travis.listTasks();
+        } else if (input.equals("bye")) {
+            travis.setIsExiting(true);
+        } else {
+            travis.addTask(input);
+        }
+    }
+}
--- src/main/java/travis/chatbot/Travis.java
+++ src/main/java/travis/chatbot/Travis.java
@@ -1,5 +1,6 @@
 package travis.chatbot;
 
+import travis.Parser;
 import travis.tasks.TaskList;
 import travis.constants.Enums;
 import travis.constants.RegexConstants;
@@ -16,19 +17,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Travis {
-    private static final Pattern markAsDonePattern = Pattern.compile(RegexConstants.MARK_AS_DONE_REGEX);
-    private static final Pattern markAsNotDonePattern = Pattern.compile(RegexConstants.MARK_AS_NOT_DONE_REGEX);
-    private static final Pattern deleteTaskPattern = Pattern.compile(RegexConstants.DELETE_TASK_REGEX);
-
-
     private final Ui ui;
     private final Storage storage;
     private final TaskList taskList;
+    private boolean isExiting;
 
     public Travis() {
         this.ui = new Ui();
         this.storage = new Storage(TaskListConstants.FILE_PATH);
         this.taskList = new TaskList();
+        this.isExiting = false;
 
         try {
             this.taskList.setTaskList(this.storage.loadTasks());
@@ -39,16 +37,11 @@ public class Travis {
         }
     }
 
-    private static void wrap(String content) {
-        System.out.println(
-                TravisConstants.HORIZONTAL_LINE + "\n" + content + "\n" + TravisConstants.HORIZONTAL_LINE + "\n");
-    }
-
-    private void listTasks() {
+    public void listTasks() {
         this.ui.listTasks(this.taskList.toString());
     }
 
-    private void addTask(String input) {
+    public void addTask(String input) {
         try {
             Task newTask = this.taskList.addTask(input);
             this.ui.notifyAddTask(newTask.toString(), this.taskList.getTaskCount());
@@ -59,7 +52,7 @@ public class Travis {
         }
     }
 
-    private void deleteTask(String taskNumberStr) {
+    public void deleteTask(String taskNumberStr) {
         try {
             int taskNumber = Integer.parseInt(taskNumberStr);
             Task deletedTask = this.taskList.deleteTask(taskNumber);
@@ -71,7 +64,7 @@ public class Travis {
         }
     }
 
-    private void markTaskAsDone(String taskNumberStr) {
+    public void markTaskAsDone(String taskNumberStr) {
         try {
             int taskNumber = Integer.parseInt(taskNumberStr);
             Task completedTask = this.taskList.markTaskAsDone(taskNumber);
@@ -83,7 +76,7 @@ public class Travis {
         }
     }
 
-    private void markTaskAsNotDone(String taskNumberStr) {
+    public void markTaskAsNotDone(String taskNumberStr) {
         try {
             int taskNumber = Integer.parseInt(taskNumberStr);
             Task incompleteTask = this.taskList.markTaskAsNotDone(taskNumber);
@@ -95,33 +88,18 @@ public class Travis {
         }
     }
 
+    public void setIsExiting(boolean isExiting) {
+        this.isExiting = isExiting;
+    }
+
     private void run() {
         this.ui.greet();
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine().trim();
 
-        while (!input.equals("bye")) {
-            Matcher markAsDoneMatcher = markAsDonePattern.matcher(input);
-            Matcher markAsNotDoneMatcher = markAsNotDonePattern.matcher(input);
-            Matcher deleteTaskMatcher = deleteTaskPattern.matcher(input);
-
-            if (markAsDoneMatcher.matches()) {
-                this.markTaskAsDone(markAsDoneMatcher.group(Enums.RegexGroup.TASK_INDEX.getGroup()));
-            } else if (markAsNotDoneMatcher.matches()) {
-                this.markTaskAsNotDone(markAsNotDoneMatcher.group(Enums.RegexGroup.TASK_INDEX.getGroup()));
-            } else if (deleteTaskMatcher.matches()) {
-                this.deleteTask(deleteTaskMatcher.group(Enums.RegexGroup.TASK_INDEX.getGroup()));
-            } else if (input.equals("list")) {
-                this.listTasks();
-            } else {
-                try {
-                    this.addTask(input);
-                } catch (InvalidTaskException e) {
-                    this.ui.warnMessage(e.getMessage());
-                }
-            }
-
+        while (!this.isExiting) {
+            Parser.parse(this, input);
             input = scanner.nextLine().trim();
         }
 
