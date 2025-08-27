fd4b520d9bd46cd15a347aabea1b48307046a30b
203ZK <houjinwork@gmail.com>
2025-08-27 14:33:03 +0800

    Simplify boolean assertions

--- src/test/java/travis/tasks/ParserTest.java
+++ src/test/java/travis/tasks/ParserTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import travis.chatbot.Parser;
 import travis.chatbot.Travis;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParserTest {
     private final static Travis travis = new Travis("");
@@ -12,60 +12,60 @@ public class ParserTest {
     @Test
     public void userCommandParsing_validList_returnsTrue() {
         boolean status = Parser.parse(travis, "list");
-        assertEquals(status, true);
+        assertTrue(status);
     }
 
     @Test
     public void userCommandParsing_invalidList_returnsFalse() {
         boolean status = Parser.parse(travis, "lisT");
-        assertEquals(status, false);
+        assertFalse(status);
     }
 
     @Test
     public void userCommandParsing_validBye_returnsTrue() {
         boolean status = Parser.parse(travis, "bye");
-        assertEquals(status, false);
+        assertTrue(status);
     }
 
     @Test
     public void userCommandParsing_invalidBye_returnsFalse() {
         boolean status = Parser.parse(travis, "by e");
-        assertEquals(status, false);
+        assertFalse(status);
     }
 
     @Test
     public void userCommandParsing_validMarkAsDone_returnsTrue() {
         boolean status = Parser.parse(travis, "mark 1");
-        assertEquals(status, true);
+        assertTrue(status);
     }
 
     @Test
     public void userCommandParsing_invalidMarkAsDone_returnsFalse() {
         boolean status = Parser.parse(travis, "mark1");
-        assertEquals(status, false);
+        assertFalse(status);
     }
 
     @Test
     public void userCommandParsing_validMarkAsNotDone_returnsTrue() {
         boolean status = Parser.parse(travis, "unmark 1");
-        assertEquals(status, true);
+        assertTrue(status);
     }
 
     @Test
     public void userCommandParsing_invalidMarkAsNotDone_returnsFalse() {
         boolean status = Parser.parse(travis, "unmark1");
-        assertEquals(status, false);
+        assertFalse(status);
     }
 
     @Test
     public void userCommandParsing_validDeleteTask_returnsTrue() {
         boolean status = Parser.parse(travis, "delete 1");
-        assertEquals(status, true);
+        assertTrue(status);
     }
 
     @Test
     public void userCommandParsing_invalidDeleteTask_returnsFalse() {
         boolean status = Parser.parse(travis, "delete1");
-        assertEquals(status, false);
+        assertFalse(status);
     }
 }
