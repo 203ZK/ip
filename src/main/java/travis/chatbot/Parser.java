e2547ce61294522f5e4e4e77bf7ef9ca0b568eb0
203ZK <houjinwork@gmail.com>
2025-08-27 14:05:59 +0800

    Add JavaDocs to Parser.java

--- src/main/java/travis/chatbot/Parser.java
+++ src/main/java/travis/chatbot/Parser.java
@@ -15,6 +15,10 @@ import java.time.format.DateTimeParseException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Responsible for parsing user inputs.
+ * The <code>Parser</code> uses regular expressions to parse the inputs, throwing exceptions if the format is invalid.
+ */
 public class Parser {
     // Patterns to match user commands
     private static final Pattern markAsDonePattern = Pattern.compile(RegexConstants.MARK_AS_DONE_REGEX);
@@ -26,6 +30,10 @@ public class Parser {
     private static final Pattern deadlinePattern = Pattern.compile(RegexConstants.DEADLINE_REGEX);
     private static final Pattern eventPattern = Pattern.compile(RegexConstants.EVENT_REGEX);
 
+    /**
+     * Parses the user's commands.
+     * If an existing command is not found, it treats the input as a new task to be added.
+     */
     public static boolean parse(Travis travis, String input) {
         Matcher markAsDoneMatcher = markAsDonePattern.matcher(input);
         Matcher markAsNotDoneMatcher = markAsNotDonePattern.matcher(input);
@@ -48,6 +56,10 @@ public class Parser {
         return true;
     }
 
+    /**
+     * Parses the task input.
+     * If the task is of an invalid format, an <code>InvalidTaskException</code> is thrown.
+     */
     public static Task parseTask(Travis travis, String input) throws InvalidTaskException {
         Matcher toDoMatcher = toDoPattern.matcher(input);
         Matcher deadlineMatcher = deadlinePattern.matcher(input);
